82191d229e9210fa36c2ad139213a6661dbaf045
Nico Ismaili <nico.ismaili@aoe.com>
2025-05-15 10:03:30 +0200

    chore(ci): disabled netlify functions during print job
    /spend 2m

--- scripts/print-pdf.js
+++ scripts/print-pdf.js
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 import { spawn } from 'child_process';
 import fs from 'fs';
 
-const MAX_RETRIES = 120; // Maximum number of retries
+const MAX_RETRIES = 60; // Maximum number of retries
 const RETRY_INTERVAL = 1000; // Retry every 1 second
 
 async function waitForServer(url, retries = 0) {
@@ -36,7 +36,7 @@ async function printCVPdf() {
   }
 
   // Start the dev server
-  const server = spawn(`npm`, [`run`, `dev`], {
+  const server = spawn(`npm`, [`run`, `dev:print`], {
     stdio: `inherit`,
     shell: true,
   });
